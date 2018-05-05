ccbe1d5ecd5de6ff193453bb940ed1d334273923
Peter Stenger <peter.a.stenger@gmail.com>
2018-05-04 21:07:35 -0400

    quizizz-bot

---
+++ README.md
@@ -0,0 +1,12 @@
+# quizizz-bot
+
+quizizz bot!
+
+Find your gameID by opening up inspect element, going to the Application tab, going to localStorage > quizizz.com > previousContext > rawData > game > gameID. Copy this when it asks for it.
+
+
+### dependencies
+
++ python3
++ selenium
++ chromedriver
---
+++ quizizz-bot.py
@@ -0,0 +1,96 @@
+from selenium import webdriver
+import time, json, requests, sys
+import helper
+from selenium.common.exceptions import TimeoutException
+def find_answers(quizID):
+    quizInfo = requests.get(f"https://quizizz.com/quiz/{quizID}/").json()
+    answers = {}
+    if "error" in quizInfo:
+        print("I couldn't find that quiz")
+        exit()
+
+    for question in quizInfo["data"]["quiz"]["info"]["questions"]:
+        if question["type"] == "MCQ":
+            if question["structure"]["options"][int(question["structure"]["answer"])]["text"] == "":
+                # image answer
+                answer = question["structure"]["options"][int(question["structure"]["answer"])]["media"][0]["url"]
+            else:
+                answer = question["structure"]["options"][int(question["structure"]["answer"])]["text"]
+        elif question["type"] == "MSQ":
+            # multiple answers
+            answer = []
+            for answerC in question["structure"]["answer"]:
+                if question["structure"]["options"][int(answerC)]["text"] == "":
+                    answer.append(question["structure"]["options"][int(answerC)]["media"][0]["url"])
+                else:
+                    answer.append(question["structure"]["options"][int(answerC)]["text"])
+        questionID = question["structure"]["query"]["text"]
+        answers[questionID] = answer
+
+    return answers
+def play(gamecode, name):
+    driver = webdriver.Chrome()
+    driver.get("https://quizizz.com/join/")
+    print("[info] starting game")
+    helper.waitForItem(driver,'.check-room-input')
+    driver.find_element_by_css_selector('.check-room-input').send_keys(gamecode)
+    driver.find_element_by_css_selector('.check-room-btn').click()
+    helper.waitForItem(driver,'.check-player-input')
+    driver.find_element_by_css_selector('.check-player-input').send_keys(name)
+    driver.find_element_by_css_selector('.left-section').find_element_by_css_selector('.menu-icon').click()
+    time.sleep(0.4)
+    driver.find_element_by_css_selector('.game-section').find_element_by_css_selector('.toggle-button').click()
+    driver.find_element_by_css_selector('.back-btn').click()
+    time.sleep(0.4)
+    driver.find_element_by_css_selector('.check-player-btn').click()
+    answers = find_answers(input("QuizID > "))
+    print("[info] answers found")
+    while True:
+        try:
+            helper.WaitForItem(driver,'.question-text-color',timeout=15)
+        except TimeoutException:
+            driver.quit()
+            break
+        try:
+            questionAnswer = answers[driver.find_element_by_css_selector('.question-text-color').get_attribute('innerHTML')]
+            choices = driver.find_element_by_css_selector('.options-container').find_elements_by_css_selector('.option')
+            firstAnswer = True
+            for answer in choices:
+                try:
+                    if isinstance(questionAnswer, list):
+                        if firstAnswer:
+                            time.sleep(0.2)
+                            firstAnswer = False
+                        if answer.find_element_by_css_selector(".resizeable").get_attribute('innerHTML') in questionAnswer:
+                            answer.click()
+                    elif answer.find_element_by_css_selector(".resizeable").get_attribute('innerHTML') == questionAnswer:
+                        answer.click()
+                        break
+                except NoSuchElementException:
+                    style = answer.find_element_by_css_selector(".option-image").get_attribute("style")
+                    if isinstance(questionAnswer, list):
+                        for correctAnswer in questionAnswer:
+                            if style in correctAnswer:
+                                answer.click()
+                                break
+                    elif questionAnswer in style:
+                        answer.click()
+                        break
+            if isinstance(questionAnswer, list):
+                driver.find_element_by_css_selector(".multiselect-submit-btn").click()
+        except KeyError:
+            input("[error] I couldn't get the answer. Please click it then hit [enter]")
+    driver.quit()
+
+if __name__ == '__main__':
+    USAGE = "quizizz-bot (INFO|PLAY)"
+    if len(sys.argv) == 1:
+       print(USAGE) 
+    elif sys.argv[1] == "INFO":
+        answers = find_answers(input("gameID >>> "))
+        for key in answers:
+            print("{} - {}".format(key, answers[key]))
+    elif sys.argv[1] == "PLAY":
+        play(input("PIN >>> "), input("username >>> "))
+    else:
+        print(USAGE)
